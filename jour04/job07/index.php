6a820da5665ba2b4aa9546eb38a9c532080c3bf4
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2023-05-25 14:47:18 +0200

    coup dur la maison

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,11 +9,48 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="get">
-        <label for="largeur">largeur</label><br>
-        <input type="text" name="largeur" id="largeur"><br>
-        <label for="longueur">longueur</label><br>
-        <input type="text" name="longueur" id="longueur"><br>
-        <input type="submit" value="Submit" name="submit">
-    </form>
+  <h2>Construction de maison</h2>
+  <form method="GET" action="">
+
+    <label for="hauteur">Hauteur :</label>
+    <input type="text" id="hauteur" name="hauteur" required><br><br>
+
+    <label for="largeur">Largeur :</label>
+    <input type="text" id="largeur" name="largeur" required><br><br>
+
+    <input type="submit" value="Construire">
+  </form>
+
+  <?php
+  if (isset($_GET['largeur']) && isset($_GET['hauteur'])) {
+      $largeur = $_GET['largeur'];
+      $hauteur = $_GET['hauteur'];
+
+      // Construction de la maison
+      $toit = "";
+      $murs = "";
+      $base = "";
+
+      // Construction du toit
+      for ($i = 0; $i < $hauteur; $i++) {
+          $toit .= str_repeat(" ", $hauteur - $i - 1) . "/" . str_repeat(" ", $i * 2) . "\\" . "\n";
+      }
+
+      // Construction des murs
+      for ($i = 0; $i < $hauteur; $i++) {
+          $murs .= "|" . str_repeat(" ", $largeur - 2) . "|" . "\n";
+      }
+
+      // Construction de la base
+      $base = "+" . str_repeat("_", $largeur - 2) . "+";
+
+      // Affichage de la maison
+      echo "<pre>";
+      echo $toit;
+      echo $murs;
+      echo $base;
+      echo "</pre>";
+  }
+  ?>
 </body>
+</html>
